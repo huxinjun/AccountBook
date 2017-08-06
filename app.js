3ec303fbf8a89f4ab14dbb50e72bbf63ccf8dc24
xinjun hu <xinjun hu>
2017-08-06 21:12:13 +0800

    主界面底部导航

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 //app.js
 App({
-  onLaunch: function() {
+  onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    wx.login({
+      success: function (res) {
+        console.log(res.code)//这就是code  
+      }
+  })
+
   },
 
-  getUserInfo: function(cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function(res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
-        }
-      })
-    }
+
+    getUserInfo: function (cb) {
+      var that = this
+      if (this.globalData.userInfo) {
+        typeof cb == "function" && cb(this.globalData.userInfo)
+      } else {
+        //调用登录接口
+        wx.getUserInfo({
+          withCredentials: true,
+          success: function (res) {
+            that.globalData.userInfo = res.userInfo
+            typeof cb == "function" && cb(that.globalData.userInfo)
+          }
+        })
+      }
+    },
+
+  test: function () {
+    wx.request({
+      url: 'http://oceanboss.tech/ABBooksServer/test/all', //仅为示例，并非真实的接口地址
+
+      success: function (res) {
+        console.log(res)
+      }
+    })
   },
 
   globalData: {
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,9 +3,38 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    motto: 'Hello World123',
+    xinjun:'hjuxinjun',
+    tab_main_src:'../img/main_press.png',
+    tab_me_src: '../img/me.png',
+    currTab:0,
+
+    text_color_home: '#2BA245',
+    text_color_me: '#272636',
+
+    userInfo: {},
+
+
+    list:[1,2,3]
+
   },
+
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh()
+    console.log("r");
+    for (let i = 0; i < 6;i++){
+      this.data.list.push(this.data.list.length);
+      console.log("push");
+    }
+    this.setData({
+      list:this.data.list
+    });
+    wx.stopPullDownRefresh();
+  },
+
+
+
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -22,5 +51,46 @@ Page({
         userInfo:userInfo
       })
     })
+    app.test()
+    
+  },
+
+  onTabMainSelected : function(){
+    console.log("选择主页了")
+  },
+  onTabMeSelected: function () {
+    console.log("选择我的了")
+
+  },
+
+  tabMainClick:function(){
+    
+    if (this.data.currTab!=0){
+      this.setData({
+        tab_main_src: '../img/main_press.png',
+        tab_me_src: '../img/me.png',
+        text_color_home: '#2BA245',
+        text_color_me: '#272636'
+
+      })
+      this.data.currTab=0;
+      this.onTabMainSelected();
+    }
+
+  },
+
+  tabMeClick: function () {
+
+    if (this.data.currTab !=1) {
+      this.setData({
+        tab_main_src: '../img/main.png',
+        tab_me_src: '../img/me_press.png',
+        text_color_home: '#272636',
+        text_color_me: '#2BA245'
+      })
+      this.data.currTab=1;
+      this.onTabMeSelected();
+    }
+
   }
 })
