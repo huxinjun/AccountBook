8993156d2d1d8c34b354ae5ab11d4f87af44d22f
xinjun <hu12315005@126.com>
2017-08-07 20:51:49 +0800

    初步完成登录,更新用户信息,获取token

--- app.js
+++ app.js
@@ -1,46 +1,93 @@
 //app.js
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    wx.login({
-      success: function (res) {
-        console.log(res.code)//这就是code  
-      }
-  })
+    onLaunch: function () {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+        // console.log("before:"+wx.getStorageSync('token'))
+        // wx.removeStorageSync('token')
+        // console.log(wx.getStorageSync('token'))
 
-  },
+    },
 
 
     getUserInfo: function (cb) {
-      var that = this
-      if (this.globalData.userInfo) {
-        typeof cb == "function" && cb(this.globalData.userInfo)
-      } else {
-        //调用登录接口
-        wx.getUserInfo({
-          withCredentials: true,
-          success: function (res) {
-            that.globalData.userInfo = res.userInfo
-            typeof cb == "function" && cb(that.globalData.userInfo)
-          }
-        })
-      }
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.getUserInfo({
+                withCredentials: false,
+                success: function (res) {
+                    that.globalData.userInfo = res.userInfo
+                    typeof cb == "function" && cb(that.globalData.userInfo)
+                    if (wx.getStorageSync('token') !='') {
+                        console.log('使用本地token')
+                        return
+                    }
+                    that.login()
+                }
+
+            })
+        }
     },
 
-  test: function () {
-    wx.request({
-      url: 'http://oceanboss.tech/ABBooksServer/test/all', //仅为示例，并非真实的接口地址
+    login:function(){
+        var that = this
+        wx.login({
+            success: function (res) {
+                //首先拿到js_code
+                console.log(res.code)//这就是code 
+                that.loginInMyServer(res.code)
+            }
+        })
+    },
 
-      success: function (res) {
-        console.log(res)
-      }
-    })
-  },
+    //在我的服务器上登录,获取token
+    loginInMyServer: function (code) {
+        console.log('loginInMyServer')
+        var that = this
+        wx.request({
+            url: 'http://192.168.10.228:8080/AccountBook/login/fromWX',
+            data: {
+                code: code
+            },
+            success: function (res) {
+                console.log(res.data)
+                that.globalData.token = res.data.token
+                wx.setStorageSync("token", res.data.token)
+                if (res.data.status == 1) {
+                    //完善个人信息
+                    that.setUserInfo();
+                }
+            }
+        })
+    },
 
-  globalData: {
-    userInfo: null
-  }
+    //第一次在我的服务器上登录,需要完善用户信息
+    setUserInfo: function () {
+        console.log('uploadUserInfo')
+        var that = this
+        wx.request({
+
+            url: 'http://192.168.10.228:8080/AccountBook/user/updateInfo',
+            data: {
+                token: that.globalData.token,
+                info: that.globalData.userInfo
+            },
+            success: function (res) {
+                console.log(res.data)
+            }
+        })
+    },
+
+
+
+    globalData: {
+        userInfo: null,
+        token: null
+    }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,95 +2,98 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World123',
-    xinjun:'hjuxinjun',
-    tab_main_src:'../img/main_press.png',
-    tab_me_src: '../img/me.png',
-    currTab:0,
-
-    text_color_home: '#2BA245',
-    text_color_me: '#272636',
-
-    userInfo: {},
-
-
-    list:[1,2,3]
-
-  },
-
-  onPullDownRefresh: function () {
-    wx.stopPullDownRefresh()
-    console.log("r");
-    for (let i = 0; i < 6;i++){
-      this.data.list.push(this.data.list.length);
-      console.log("push");
-    }
-    this.setData({
-      list:this.data.list
-    });
-    wx.stopPullDownRefresh();
-  },
-
-
-
-
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-    app.test()
-    
-  },
-
-  onTabMainSelected : function(){
-    console.log("选择主页了")
-  },
-  onTabMeSelected: function () {
-    console.log("选择我的了")
-
-  },
-
-  tabMainClick:function(){
-    
-    if (this.data.currTab!=0){
-      this.setData({
+    data: {
+        motto: 'Hello World123',
+        xinjun: 'hjuxinjun',
         tab_main_src: '../img/main_press.png',
         tab_me_src: '../img/me.png',
+        currTab: 0,
+
         text_color_home: '#2BA245',
-        text_color_me: '#272636'
+        text_color_me: '#272636',
 
-      })
-      this.data.currTab=0;
-      this.onTabMainSelected();
+        userInfo: {},
+
+
+        list: [1, 2, 3]
+
+    },
+
+    onPullDownRefresh: function () {
+        wx.stopPullDownRefresh()
+        console.log("r");
+        for (let i = 0; i < 6; i++) {
+            this.data.list.push(this.data.list.length);
+            console.log("push");
+        }
+        this.setData({
+            list: this.data.list
+        });
+        wx.stopPullDownRefresh();
+    },
+
+
+
+
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+
+
+    
+    onLoad: function () {
+        console.log('onLoad')
+        var that = this
+        
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function (userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
+
+    },
+
+    onTabMainSelected: function () {
+        console.log("选择主页了")
+    },
+    onTabMeSelected: function () {
+        console.log("选择我的了")
+
+    },
+
+    tabMainClick: function () {
+
+        if (this.data.currTab != 0) {
+            this.setData({
+                tab_main_src: '../img/main_press.png',
+                tab_me_src: '../img/me.png',
+                text_color_home: '#2BA245',
+                text_color_me: '#272636'
+
+            })
+            this.data.currTab = 0;
+            this.onTabMainSelected();
+        }
+
+    },
+
+    tabMeClick: function () {
+
+        if (this.data.currTab != 1) {
+            this.setData({
+                tab_main_src: '../img/main.png',
+                tab_me_src: '../img/me_press.png',
+                text_color_home: '#272636',
+                text_color_me: '#2BA245'
+            })
+            this.data.currTab = 1;
+            this.onTabMeSelected();
+        }
+
     }
-
-  },
-
-  tabMeClick: function () {
-
-    if (this.data.currTab !=1) {
-      this.setData({
-        tab_main_src: '../img/main.png',
-        tab_me_src: '../img/me_press.png',
-        text_color_home: '#272636',
-        text_color_me: '#2BA245'
-      })
-      this.data.currTab=1;
-      this.onTabMeSelected();
-    }
-
-  }
 })
