3de9c6890a91a3eeaea4bc233e0fb07cb297e122
xinjun <hu12315005@126.com>
2017-09-17 17:30:50 +0800

    分组头像绘制

--- pages/group_edit/group_edit.js
+++ pages/group_edit/group_edit.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 var slider
+var imageutil = require('../../utils/imageutil.js')
 var APP = getApp()
 Page({
     data: {
@@ -41,11 +42,75 @@ Page({
 
 
 
-    onLoad: function () {
+    onLoad: function (option) {
         console.log('onLoad')
         var that = this
-        
+        // if(option.isAddGroup)
+        //   this.changeButtonStatus("add_group_info_invalid")
+        // else
+        this.changeButtonStatus("add_group_info_invalid")
+
+        const canvas = wx.createCanvasContext('canvas')
+
+
+        imageutil.drawIcon(canvas,'','',{
+            success:function(localFile){
+                that.setData({
+                    group_icon:localFile
+                })
+            }
+        })
     },
+
+    changeButtonStatus:function(status){
+        switch(status){
+          case "add_group_info_invalid":
+            this.setData({
+              commit_class: "commit_disable",
+              commit_text:"新增分组(红点项必填）",
+              deleteStyle: "display:none;",
+              quitStyle: "display:none;",
+            })
+            break;
+          case "add_group_info_valid": 
+            this.setData({
+              commit_class: "commit",
+              commit_text: "新增分组",
+              commit_hover_class:"commit_hover",
+              deleteStyle: "display:none;",
+              quitStyle: "display:none;",
+            })
+            break;
+          case "update_admin_member":
+            this.setData({
+              commit_class: "commit",
+              commit_text: "提交更改",
+              commit_hover_class: "commit_hover",
+              deleteStyle: "",
+              quitStyle: "",
+            })
+            break;
+          case "update_admin":
+            this.setData({
+              commit_class: "commit",
+              commit_text: "提交更改",
+              commit_hover_class: "commit_hover",
+              deleteStyle: "",
+              quitStyle: "display:none;",
+            })
+            break;
+          case "update_member":
+            this.setData({
+              commit_class: "commit",
+              commit_text: "提交更改",
+              commit_hover_class: "commit_hover",
+              deleteStyle: "display:none;",
+              quitStyle: "",
+            })
+            break;
+
+        }
+    }
 
 })
 
--- utils/util.js
+++ utils/util.js
@@ -15,7 +15,37 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+/**
+ * rpx转换为px单位
+ */
+function rpx2px(rpx) {
+  var px=0
+  wx.getSystemInfo({
+    success: function (res) {
+    //   console.log(res)
+      px = res.screenWidth / 750 * rpx
+    }
+  })
+  console.log("rpx" + rpx+"=px"+px)
+  return px;
+}
+/**
+ * px转换为rpx单位
+ */
+function px2rpx(px) {
+  var rpx = 0
+  wx.getSystemInfo({
+    success: function (res) {
+    //   console.log(res)
+      rpx = px / res.screenWidth * 750
+    }
+  })
+  console.log("px" + px + "=rpx" + rpx)
+  return rpx;
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  rpx2px: rpx2px,
+  px2rpx: px2rpx
 }
