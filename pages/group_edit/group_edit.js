183db39fc2770b5a658bf1b21f200ffe18f97cc2
xinjun <hu12315005@126.com>
2017-09-20 19:22:24 +0800

    分组信息更新ing

--- pages/group_edit/group_edit.js
+++ pages/group_edit/group_edit.js
@@ -5,13 +5,15 @@ var APP = getApp()
 var dialog = require("../../utils/dialog.js")
 Page({
     data: {
+        ImageBaseUrl: APP.globalData.BaseUrl +"/image/get/",
 
         groupInfo: {
             group: {
                 name: "",
-                desc: ""
+                desc: "",
+                icon: null
             },
-            members:[]
+            users:[]
         }
 
     },
@@ -29,6 +31,20 @@ Page({
             },
 
             success: function (res) {
+                this.setData({
+                    groupInfo:res.data
+                })
+                console.log(res.data.isAdmin + "............." + res.data.isMember)
+                if (res.data.isAdmin && res.data.isMember)
+                    this.changeButtonStatus("update_admin_member")
+                else if (res.data.isAdmin)
+                    this.changeButtonStatus("update_admin")
+                else if (res.data.isMember)
+                    this.changeButtonStatus("update_member")
+
+                this.setMemberVisible(true)
+                this.setNameValid(true)
+                this.setDescValid(true)
             }
 
         }, this)
@@ -38,6 +54,7 @@ Page({
      * 新增分组
      */
     addGroup: function (e) {
+        console.log("addGroup")
         APP.ajax({
             url: APP.globalData.BaseUrl + '/group/add',
 
@@ -52,6 +69,36 @@ Page({
             }
 
         }, this)
+    },
+
+    /**
+     * 更新分组信息
+     */
+    updateGroup: function (e) {
+        console.log("updateGroup")
+        APP.ajax({
+            url: APP.globalData.BaseUrl + '/group/update',
+
+            data: {
+                token: wx.getStorageSync("token"),
+                groupId: this.data.groupInfo.group.id,
+                name: this.data.groupInfo.group.name,
+                desc: this.data.groupInfo.group.desc
+            },
+
+            success: function (res) {
+               
+            }
+
+        }, this)
+    },
+
+
+    /**
+     * 添加成员
+     */
+    addMember: function (e) {
+
     },
 
 
@@ -76,10 +123,16 @@ Page({
             nameValid: valid,
             namePointStyle: valid ? "display:none;" : "display:inherit;"
         })
-        if (this.data.nameValid && this.data.descValid)
-            this.changeButtonStatus("add_group_info_valid")
-        else
-            this.changeButtonStatus("add_group_info_invalid")
+        var isAddGroup=this.data.groupInfo.group.id==undefined
+        var isAllValid = this.data.nameValid && this.data.descValid
+        this.commitEnable(isAllValid, isAddGroup ? "addGroup" :"updateGroup")
+
+        if (isAddGroup)
+            if (isAllValid)
+                this.changeButtonStatus("add_group_info_valid")
+            else
+                this.changeButtonStatus("add_group_info_invalid")
+
     },
     /**
      * 显示或隐藏分类条目的小红点
@@ -89,10 +142,25 @@ Page({
             descValid: valid,
             descPointStyle: valid ? "display:none;" : "display:inherit;"
         })
-        if (this.data.nameValid && this.data.descValid)
-            this.changeButtonStatus("add_group_info_valid")
-        else
-            this.changeButtonStatus("add_group_info_invalid")
+        var isAddGroup = this.data.groupInfo.group.id == undefined
+        var isAllValid = this.data.nameValid && this.data.descValid
+        this.commitEnable(isAllValid, isAddGroup ? "addGroup" : "updateGroup")
+
+        if (isAddGroup)
+            if (isAllValid)
+                this.changeButtonStatus("add_group_info_valid")
+            else
+                this.changeButtonStatus("add_group_info_invalid")
+    },
+
+    /**
+     * 设置是否显示成员
+     * 添加分组时不显示
+     */
+    setMemberVisible: function (visible) {
+        this.setData({
+            membersStyle: visible ? "" : "display:none;"
+        })
     },
 
 
@@ -101,14 +169,28 @@ Page({
     onLoad: function (option) {
         console.log('onLoad')
         var that = this
-        
+        this.setMemberVisible(false)
+        option.groupId="123"
         if (!option.groupId) {
             //添加分组
             this.changeButtonStatus("add_group_info_invalid")
+            this.commitEnable(false)
         } else{
             //编辑分组
             this.pullGroupInfo(option.groupId)
         }
+    },
+
+    /**
+     * 提交按钮的开启关闭和对应的点击事件
+     */
+    commitEnable: function (enable,funcName) {
+        console.log("commitEnable:" + enable + "-----------" + funcName)
+        this.setData({
+            commit_class: enable ?"commit":"commit_disable",
+            commit_click: enable ? funcName:"",
+            commit_hover_class: enable ?"commit_hover":""
+        })
     },
 
     /**
@@ -118,54 +200,39 @@ Page({
         switch (status) {
             case "add_group_info_invalid":
                 this.setData({
-                    commit_class: "commit_disable",
                     commit_text: "新增分组(红点项必填）",
-                    commit_click:"",
-                    membersStyle:"display:none;",
                     deleteStyle: "display:none;",
                     quitStyle: "display:none;",
                 })
                 break;
             case "add_group_info_valid":
                 this.setData({
-                    commit_class: "commit",
                     commit_text: "新增分组",
-                    commit_click: "addGroup",
-                    commit_hover_class: "commit_hover",
-                    membersStyle: "display:none;",
                     deleteStyle: "display:none;",
                     quitStyle: "display:none;",
                 })
                 break;
             case "update_admin_member":
                 this.setData({
-                    commit_class: "commit",
                     commit_text: "提交更改",
-                    commit_click: "updateGroup",
-                    commit_hover_class: "commit_hover",
-                    membersStyle: "",
-                    deleteStyle: "",
-                    quitStyle: "",
+                    deleteStyle: "display:block;",
+                    quitStyle: "display:block;"
                 })
                 break;
             case "update_admin":
+                console.log("update_admin")
+                console.log(this)
                 this.setData({
-                    commit_class: "commit",
                     commit_text: "提交更改",
-                    commit_hover_class: "commit_hover",
-                    membersStyle: "",
-                    deleteStyle: "",
+                    deleteStyle: "display:block;",
                     quitStyle: "display:none;",
                 })
                 break;
             case "update_member":
                 this.setData({
-                    commit_class: "commit",
                     commit_text: "提交更改",
-                    commit_hover_class: "commit_hover",
-                    membersStyle: "",
                     deleteStyle: "display:none;",
-                    quitStyle: "",
+                    quitStyle: "display:block;"
                 })
                 break;
 
@@ -233,31 +300,20 @@ Page({
         dialog.showDialog(dialogInfo)
     },
 
-    
-
-    /**
-     * 更新分组信息
-     */
-    updateGroup: function (e) {
-
-    },
-
-    /**
-     * 添加成员
-     */
-    addMember: function (e) {
-
-    },
 
     /**
      * 成员按下,进行动画
      */
     memberTouchDown:function(e){
-        if(e.target.dataset.index)
+        var index = e.target.dataset.index
+        console.log(index)
+        if (index!=undefined){
+            var member = this.data.groupInfo.users[index]
+            member.memberAnim = "transform:scale(0.9,0.9);"
             this.setData({
-                memberAnim: "transform:scale(0.9,0.9);"
+                groupInfo:this.data.groupInfo
             })
-        else
+        }else
             this.setData({
                 memberAddAnim:"transform:scale(0.9,0.9);"
             })
@@ -267,11 +323,15 @@ Page({
      * 添加成员按钮按下,进行动画
      */
     memberTouchUp: function (e) {
-        if (e.target.dataset.index)
+        var index = e.target.dataset.index
+
+        if (index != undefined) {
+            var member = this.data.groupInfo.users[index]
+            member.memberAnim = "transform:scale(1,1);"
             this.setData({
-                memberAnim: "transform:scale(1,1);"
+                groupInfo: this.data.groupInfo
             })
-        else
+        } else
             this.setData({
                 memberAddAnim: "transform:scale(1,1);"
             })
