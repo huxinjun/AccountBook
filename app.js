fae4dd6c29c7463fcc8f6086e331586d306e415d
xinjun <hu12315005@126.com>
2017-12-08 19:07:00 +0800

    添加真实服务器地址

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 var util = require("/utils/util.js")
 App({
     globalData: {
-        BaseUrl: 'http://192.168.10.17:8080/AccountBook',
+        BaseUrl: 'http://xzbenben.cn/AccountBook',
+        //BaseUrl: 'http://192.168.10.17:8080/AccountBook',
         // BaseUrl: 'http://127.0.0.1:8080/AccountBook',
         // BaseUrl: 'http://oceanboss.tech/AccountBook',
         // BaseUrl: 'http://192.168.1.103:8080/AccountBook',
